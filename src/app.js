5b4a3371acaa29807d766814cbc1b2067b4f1cb4
Abiodun-Shittu <jerrywizklay1@gmail.com>
2022-05-29 13:14:57 +0100

    Create an Account

--- src/app.js
+++ src/app.js
@@ -1,9 +1,14 @@
 const express = require("express");
 
+// Import router
+const userRouter = require('../routes/users.js')
+
 // Express App
 const app = express();
-
 const port = 8000;
+
+app.use(express.json());
+app.use('/api/v0/ping', userRouter);
 
 app.get('/api/v0/ping', (req, res) => {
     res.send("ToDo Application is Live")
