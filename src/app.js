ee09c6b1a3dbb1f25ca02f5a12e0b63603b689c9
Abiodun-Shittu <jerrywizklay1@gmail.com>
2022-05-30 11:28:31 +0100

    Update User Endpoints

--- src/app.js
+++ src/app.js
@@ -1,18 +1,18 @@
 const express = require("express");
 
 // Import router
-const userRouter = require('../routes/users.js')
+const userRoutes = require('../routes/users.js');
 
 // Express App
 const app = express();
 const port = 8000;
 
 app.use(express.json());
-app.use('/api/v0/ping', userRouter);
+app.use('/api/v0', userRoutes);
 
 app.get('/api/v0/ping', (req, res) => {
     res.send("ToDo Application is Live")
-})
+});
 
 // Listen For Request
 app.listen(port, () => {
