81a34584e235294e4df69d1d1e52d5fb86e45766
Abiodun-Shittu <jerrywizklay1@gmail.com>
2022-05-30 11:28:31 +0100

    Update User Endpoints

--- routes/users.js
+++ routes/users.js
@@ -9,13 +9,13 @@ let users = [];
 // Find all users
 router.get('/users', (req, res) => {
     res.json(users)
-})
+});
 
 // Create users
-router.post('/create', (req, res) => {
+router.post('/users', (req, res) => {
     const user = req.body;
     users.push(user);
-    res.send(`You account has been successfully created`);
+    res.send(`Your account has been successfully created`);
 });
 
 // Export Router
