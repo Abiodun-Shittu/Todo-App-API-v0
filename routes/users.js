845b711d960bdfe69818dee760a0d1fa593e0ecf
Abiodun-Shittu <jerrywizklay1@gmail.com>
2022-06-08 23:03:20 +0100

    MVC folder structure approach

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const { use } = require("express/lib/application");
-const { v4: uuidv4 } = require('uuid');
+const userController = require('../controllers/userController');
 
 // Initaialize router
 const router = express.Router();
@@ -19,60 +19,24 @@ const validateUser = (req, res, next) => {
     } else {
         next();
     }
-}
-
-// Create an array to store user data
-let users = [];
+};
 
 // Find all users
-router.get('/users', (req, res) => {
-    res.json(users)
-});
+router.get('/users', userController.getUsers);
 
 // Create users
-router.post('/users', validateUser, (req, res) => {
-    const name = req.body.name;
-    const email = req.body.email;
-    const user = {
-        id: uuidv4(),
-        email,
-        name,
-    };
-    users.push(user);
-    res.status(200).json(user);
-});
+router.post('/users', validateUser, userController.createUser);
 
 // Find a Specific User with Id
-router.get('/users/:id', (req, res) => {
-    const id = req.params.id;
-    const findUser = users.find((user) => user.id === id);
-    res.json(findUser);
-});
+router.get('/users/:id', userController.getUser);
 
 // Update User Details
 // .put() can also be used to update the user details that is if you want to completely change all the details
 // .patch() is used in order to just change one of the details of the user
-router.patch('/users/:id', (req, res) => {
-    const id = req.params.id;
-    const name = req.body.name;
-    const email = req.body.email;
-    const updateUser = users.find((user) => user.id === id);
-    if (name) {
-        updateUser.name = name;
-    }
-    if (email) {
-        updateUser.email = email;
-    }
-
-    res.json(updateUser);
-})
+router.patch('/users/:id', userController.updateUser);
 
 // Delete User
-router.delete('/users/:id', (req, res) => {
-    const id = req.params.id;
-    users = users.filter((user) => user.id !== id);
-    res.json(users);
-})
+router.delete('/users/:id', userController.deleteUser);
 
 // Export Router
 module.exports = router;
