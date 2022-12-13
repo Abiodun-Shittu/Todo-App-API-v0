dac7020dd61843e38f95295bef217a42fb578b39
Abiodun Shittu <jerrywizklay1@gmail.com>
2022-12-13 17:07:50 +0100

    Fix todo queries

--- src/modules/todos/queries.js
+++ src/modules/todos/queries.js
@@ -1,9 +1,9 @@
 const allTodos =
-	"SELECT todo_id, title, status, due_date, created_at, updated_at FROM todos WHERE user_id = $1";
+	"SELECT todo_id, title, description, status, due_date, created_at, updated_at FROM todos WHERE user_id = $1";
 const newTodo =
-	"INSERT INTO todos (todo_id, user_id, title, status, due_date, created_at, updated_at) VALUES ($1, $2, $3, $4, $5, $6, $7)";
+	"INSERT INTO todos (todo_id, user_id, title, description, due_date, created_at, updated_at) VALUES ($1, $2, $3, $4, $5, $6, $7)";
 const findTodo =
-	"SELECT todo_id, user_id, title, status, due_date, created_at, updated_at FROM todos WHERE todo_id = $1";
+	"SELECT todo_id, user_id, title, description, status, due_date, created_at, updated_at FROM todos WHERE todo_id = $1";
 const deleteTodo = "DELETE FROM todos WHERE todo_id = $1";
 
 export default {
